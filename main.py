4e0f52413588681dab095bc312294441cc78573e
BananaOfHappiness <oneshanks1@gmail.com>
2024-12-03 14:53:03 +0300

    add game class and simple game loop

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print("Hello, World!")
-print('Hello Egor check this out!!!')
-a = 21
+import player
+from enum import Enum, auto
+
+
+class GameState(Enum):
+    RUNNING = auto()
+    EXITING = auto()
+
+class TurnState(Enum):
+    IN_BATTLE = auto()
+
+
+class Game:
+    def __init__(self):
+        self.state: GameState = GameState.RUNNING
+
+
+if __name__ == "__main__":
+    game = Game()
+    while game.state == GameState.RUNNING:
+        user_input: str = input()
+        if user_input.lower() == "выйти":
+            game.state = GameState.EXITING
