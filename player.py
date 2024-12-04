d52b18289f8d8ddfa4d6d4ceffc5627abd18c655
BananaOfHappiness <oneshanks1@gmail.com>
2024-12-04 10:36:59 +0300

    add use_spell()

--- player.py
+++ player.py
@@ -1,4 +1,6 @@
 from Items import Item
+from Spells import Spell
+
 
 class Player:
     def __init__(self, name):
@@ -8,6 +10,7 @@ class Player:
         self.mana: int = 10
         self.iq: int = 10
         self.items: list[Item] = []
+        self.spells: list[Spell] = []
 
     def use_item(self):
         if len(self.items) == 0:
@@ -31,6 +34,29 @@ class Player:
                 self.items[choice - 1].use()
             except IndexError:
                 print("Нет такого предмета, попробуйте снова")
+
+    def use_spell(self):
+        if len(self.spells) == 0:
+            print("У Вас нет заклинаний")
+            return
+
+        while True:
+            print("Выберите заклинание из списка цифрой:")
+            print("0: Отмена")
+            for i, spell in enumerate(self.spells):
+                print(f"{i + 1}: {spell}")
+
+            try:
+                choice = int(input())
+            except ValueError:
+                print("Вы ввели не число, попробуйсте снова")
+
+            if choice == 0:
+                break
+            try:
+                self.spells[choice - 1].use()
+            except IndexError:
+                print("Нет такого заклинания, попробуйте снова")
 
 
 class Wizard(Player):
