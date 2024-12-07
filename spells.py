74ef17975c61bf6bb742418b8f107d385e2f35a1
ariky <140708975+kurmyshkina-arina@users.noreply.github.com>
2024-12-07 13:41:57 +0300

    Update spells.py

--- spells.py
+++ spells.py
@@ -1,4 +1,6 @@
 import main
+import threading
+import time
 
 
 class Spells():
@@ -52,11 +54,14 @@ class Swallow(Spells):
         self.desc = 'Здоровье персонажа увеличивается на 20 ХП на 1 атаку'
         self.value = 1
 
-    def use(self, character, enemy):
+    def use(self, character):
         super().use(character)
         character.hp += 20
-        character.attack(enemy)
-        enemy.attack(character)
+        print(f"Здоровье увеличено до {character.hp}")
+        threading.Thread(target=self.reset_health).start()
+
+    def reset_health(self, character):
+        time.sleep(60)
         character.hp -= 20
 
 
@@ -67,12 +72,15 @@ class Invulnerability(Spells):
         self.desc = 'Персонаж с большей вероятностью увернется от атаки'
         self.value = 1
 
-    def use(self, character, enemy):
+    def use(self, character):
         super().use(character)
-        character.dodge += 3
-        character.attack(enemy)
-        enemy.attack(character)
-        character.dodge -= 3
+        character.dodge += 10
+        print(f"Уклонение увеличено до {character.dodge}")
+        threading.Thread(target=self.reset_dodge).start()
+
+    def reset_dodge(self, character):
+        time.sleep(60)
+        character.dodge -= 10
 
 
 class Distant_Attack(Spells):
